80e0d27f031499af2a5baa111ddcd838d4e054bf
demon-ora <softwareselling26@gmail.com>
2023-02-27 17:17:46 +0545

    added navbar and react router dom with its elemtent

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Navbar from "./component/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Login from "./component/Login";
+import Home from "./component/Home";
+import Signup from "./component/Signup";
+import About from "./component/About";
+import Contact from "./component/Contact";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 };
--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href=" ">
+          <NavLink className="navbar-brand" to="/">
             Navbar
-          </a>
+          </NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,34 +23,29 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href=" ">
+                <NavLink className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href=" ">
+                <NavLink className="nav-link" to="/about">
                   About
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href=" ">
+                <NavLink className="nav-link" to="/contact">
                   Contact
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href=" ">
+                <NavLink className="nav-link" to="/login">
                   Login
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href=" ">
+                <NavLink className="nav-link" to="/signup">
                   Signup
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href=" ">
-                  Link
-                </a>
+                </NavLink>
               </li>
             </ul>
           </div>
